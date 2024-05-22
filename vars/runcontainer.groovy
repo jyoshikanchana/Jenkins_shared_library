d936d478943b3e33c4ba752d7988e4d0a02419ba
jyoshikanchana <142913400+jyoshikanchana@users.noreply.github.com>
2024-05-22 13:05:55 +0530

    Update runcontainer.groovy

--- vars/runcontainer.groovy
+++ vars/runcontainer.groovy
@@ -1,3 +1,3 @@
 def call() {
-    sh 'docker run -d --name youtube2 -p 3000:3000 devopsraju6/youtube2:latest'
+    sh 'docker run -d --name ${imageName} -p 3000:3000 ${dockerHubUsername}/${imageName}:latest'
 }
